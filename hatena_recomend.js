61d7579b28f1b7b3113c78f8933d00774ded190a
sumple_html <nori559259@gmail.com>
2016-12-29 12:33:56 +0900

    add file hatena_recomend

---
+++ hatena_recomend.js
@@ -0,0 +1,7 @@
+$.get(http://noriyasu-katano.hatenablog.com/rss, function (data) {
+    $(data).find("item").each(function () {
+        var el = $(this);
+          console.log(el.find("link").text());
+          console.log(el.find("title").text());
+    });
+});
