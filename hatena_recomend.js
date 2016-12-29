4971513d337fa4cc92949be826ca7f6a3e04e6e3
sumple_html <nori559259@gmail.com>
2016-12-29 12:36:59 +0900

    fix01

--- hatena_recomend.js
+++ hatena_recomend.js
@@ -1,4 +1,4 @@
-$.get(http://noriyasu-katano.hatenablog.com/rss, function (data) {
+$.get("http://noriyasu-katano.hatenablog.com/rss", function (data) {
     $(data).find("item").each(function () {
         var el = $(this);
           console.log(el.find("link").text());
